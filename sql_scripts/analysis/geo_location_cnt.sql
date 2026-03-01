b5bb44fc6b1f7364ffee4e9fc5cd988c9d47af72
Denimwhyte <karenlashley@live.co.uk>
2026-03-01 22:51:17 +0000

    Delete sql_scripts/analysis/geo_location_cnt.sql

--- sql_scripts/analysis/geo_location_cnt.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-  geo_latitude,
-  geo_longitude,
-  COUNT(*) AS total_transactions
-FROM `aml-capstone-project.aml_project.fact_transactions`
-GROUP BY geo_latitude, geo_longitude
-ORDER BY total_transactions DESC;
