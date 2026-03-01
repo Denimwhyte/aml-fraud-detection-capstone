b7a93033178a208927ffd47a8fc717f8878bda28
Denimwhyte <karenlashley@live.co.uk>
2026-03-01 22:50:40 +0000

    Delete sql_scripts/analysis/network_slice_cnt.sql

--- sql_scripts/analysis/network_slice_cnt.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-  network_slice_id,
-  COUNT(*) AS total_transactions
-FROM `aml-capstone-project.aml_project.fact_transactions`
-GROUP BY network_slice_id
-ORDER BY total_transactions DESC;
