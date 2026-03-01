404dbed31641e6ac13b5662d3684a8f61b5a6b5f
Denimwhyte <karenlashley@live.co.uk>
2026-03-01 23:03:30 +0000

    Delete sql_scripts/transformations/dim_status.sql

--- sql_scripts/transformations/dim_status.sql
+++
@@ -1,5 +0,0 @@
-CREATE OR REPLACE TABLE `aml-capstone-project.aml_project.dim_status` AS
-SELECT DISTINCT
-  transaction_status
-FROM `aml-capstone-project.aml_project.stg_transactions`
-ORDER BY transaction_status;
