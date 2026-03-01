af375bed2cc5f96d41aa25256f4e5c3329dc5037
Denimwhyte <karenlashley@live.co.uk>
2026-03-01 22:51:06 +0000

    Delete sql_scripts/analysis/hour_timestamp_cnt.sql

--- sql_scripts/analysis/hour_timestamp_cnt.sql
+++
@@ -1,7 +0,0 @@
-#minute data discovered
-SELECT
-  EXTRACT(HOUR FROM timestamp) AS hour,
-  COUNT(*) AS transactions
-FROM `aml-capstone-project.aml_project.fact_transactions`
-GROUP BY hour
-ORDER BY hour;
