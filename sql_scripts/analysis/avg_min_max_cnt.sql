36b54ae6d044106d37b2de140e72400806561de9
Denimwhyte <karenlashley@live.co.uk>
2026-03-01 22:51:40 +0000

    Delete sql_scripts/analysis/avg_min_max_cnt.sql

--- sql_scripts/analysis/avg_min_max_cnt.sql
+++
@@ -1,5 +0,0 @@
-SELECT
-  AVG(transaction_amount) AS average_amount,
-  MIN(transaction_amount) AS minimum_amount,
-  MAX(transaction_amount) AS maximum_amount
-FROM `aml-capstone-project.aml_project.fact_transactions`;
