1c46c77e2d34a50e1bfb36ea9c3f83ec7c44ff74
Denimwhyte <karenlashley@live.co.uk>
2026-03-01 23:09:54 +0000

    Delete sql_scripts/transformations/dim_device.sql

--- sql_scripts/transformations/dim_device.sql
+++
@@ -1,5 +0,0 @@
-CREATE OR REPLACE TABLE `aml-capstone-project.aml_project.dim_device` AS
-SELECT DISTINCT
-  device_used AS device_name
-FROM `aml-capstone-project.aml_project.stg_transactions`
-ORDER BY device_name;
