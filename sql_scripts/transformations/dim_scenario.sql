ac3577c8e4fd5ca7c91b82b3a41b1e91e01e4eed
Denimwhyte <karenlashley@live.co.uk>
2026-03-01 23:04:35 +0000

    Delete sql_scripts/transformations/dim_scenario.sql

--- sql_scripts/transformations/dim_scenario.sql
+++
@@ -1,5 +0,0 @@
-CREATE OR REPLACE TABLE `aml-capstone-project.aml_project.dim_scenario` AS
-SELECT DISTINCT
-  scenario_type
-FROM `aml-capstone-project.aml_project.stg_transactions`
-ORDER BY scenario_type;
