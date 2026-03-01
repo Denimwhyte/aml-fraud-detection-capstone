44ad6b04e6fa3a85b16757e07a639bca562a24f8
Denimwhyte <karenlashley@live.co.uk>
2026-03-01 22:50:10 +0000

    Delete sql_scripts/analysis/success_fail_cnt.sql

--- sql_scripts/analysis/success_fail_cnt.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-  transaction_status,
-  COUNT(*) AS total_transactions
-FROM `aml-capstone-project.aml_project.fact_transactions`
-GROUP BY transaction_status
-ORDER BY total_transactions DESC;
